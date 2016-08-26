5294a77a2ae45e8813a793ffc29cd8d31835b46b
xteeven <david7246@gmail.com>
2016-08-26 11:48:36 -0500

    Segmentation and Read in separated files

--- Bin/Read.py
+++ Bin/Read.py
@@ -1,10 +1,15 @@
-import cv2
+"""
+Conjunto de funciones Aplicadas para Leer Organizar y buscar Carpetas Y Directorios
+Contiene:
+Lectura Raw
+Busqueda En Directorios
+Crear Matrices
+"""
+
+
 import os
 import numpy as np
 from PIL import Image
-import time
-start_time = time.time()
-
 
 
 def rawRead(path):
@@ -16,6 +21,19 @@ def rawRead(path):
     imagenRaw = np.asanyarray(imagenRaw, dtype='uint16')
     imagenRaw *= (2 ** 16) / imagenRaw.max()
     return imagenRaw
+
+def search(pathe, folder):
+    """
+    Busqueda De carpetas al interior de los directorios que contienen las imagenes
+    :pathe: Directorio en el cual se desea buscar
+    :folder: Nombre de la carpeta que se desea encontrar
+    Devuelve array con la totalidad de coincidencias encontradas
+    """
+
+    directorios = [x[0] if x[0].split('\\')[-1] == folder else '' for x in os.walk(pathe)]
+    while '' in directorios:
+        directorios.remove('')
+    return directorios
 
 def createMatrix(path):
     """"
@@ -38,52 +56,4 @@ def createMatrix(path):
         imagematrix.append(imagerows)
     return imagematrix
 
-def avgPol(matrix, polarizacion):
-    """
-    :param matrix: Matriz generada con el metodo createMatrix
-    :param polarizacion: [0-2] tipo de polarizacion
-    :return:
-    Devuelve la intensidad promedio para una polarizacion especifica
-    """
-    if polarizacion<3:
-        outprom = matrix[0][0] * 0
-        for i in range(0, 8):
-            outprom += matrix[polarizacion][i] / 8
-        return outprom
-    else:
-        avgotsu = uint16to8(matrix[0][0] * 0)
-        for i in range(0, 3):
-            outsu = avgPol(matrix, i)
-            outsu = uint16to8(outsu)
-            a, outsu = cv2.threshold(outsu, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-            avgotsu += outsu / 3
-        return avgotsu
 
-def uint16to8(inputt): return np.asanyarray(inputt/250, dtype="uint8")
-
-def search(pathe, folder):
-    """
-    Busqueda De carpetas al interior de los directorios que contienen las imagenes
-    :pathe: Directorio en el cual se desea buscar
-    :folder: Nombre de la carpeta que se desea encontrar
-    Devuelve array con la totalidad de coincidencias encontradas
-    """
-
-    directorios = [x[0] if x[0].split('\\')[-1] == folder else '' for x in os.walk(pathe)]
-    while '' in directorios:
-        directorios.remove('')
-    return directorios
-
-
-"""Carpetas 008, B007, 018, 030, 029"""
-
-
-path = search('Data/022', 'multi')[0]
-
-imatrix = createMatrix(path)
-avgotsu = avgPol(imatrix,8)
-cv2.imshow('1', imatrix[2][2])
-#cv2.imshow('2', avgotsu)
-
-print("--- %s seconds ---" % (time.time() - start_time))
-cv2.waitKey()
---
+++ Bin/Segment.py
@@ -0,0 +1,49 @@
+"""
+Programa de Segmentacion
+Contiene
+"""
+import cv2
+import numpy as np
+from Read import search, createMatrix
+import time
+
+start_time = time.time()
+
+def avgPol(matrix, polarizacion):
+    """
+    :param matrix: Matriz generada con el metodo createMatrix
+    :param polarizacion: [0-2] tipo de polarizacion
+    :return:
+    Devuelve la intensidad promedio para una polarizacion especifica
+    """
+    if polarizacion<3:
+        outprom = matrix[0][0] * 0
+        for i in range(0, 8):
+            outprom += matrix[polarizacion][i] / 8
+        return outprom
+    else:
+        avgotsu = uint16to8(matrix[0][0] * 0)
+        for i in range(0, 3):
+            outsu = avgPol(matrix, i)
+            outsu = uint16to8(outsu)
+            a, outsu = cv2.threshold(outsu, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+            avgotsu += outsu / 3
+        return avgotsu
+
+def uint16to8(inputt): return np.asanyarray(inputt/250, dtype="uint8")
+
+"""Carpetas 008, B007, 018, 030, 029"""
+
+
+path = search('Data/022', 'multi')[0]
+
+imatrix = createMatrix(path)
+avgotsu = avgPol(imatrix,8)
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+cv2.imshow('1', imatrix[0][2])
+#cv2.imshow('2', avgotsu)
+
+cv2.waitKey()
